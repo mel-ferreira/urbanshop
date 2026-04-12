22c9713bd720c636bfcbfcb9520e15fbe8e711f0
mel-ferreira <melissamouraf@gmail.com>
2026-04-11 23:59:33 -0300

    [FEATURE] Implementar Migrations e Seeding de tabelas

--- urbanshop.ProductApi/Context/ProdutoDbContext.cs
+++ urbanshop.ProductApi/Context/ProdutoDbContext.cs
@@ -7,15 +7,68 @@ using urbanshop.ProductApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
-namespace urbanshop.ProductApi.Context
+namespace urbanshop.ProductApi.Context;
+public class ProdutoDbContext : DbContext
 {
-    public class ProdutoDbContext : DbContext
+    public ProdutoDbContext(DbContextOptions<ProdutoDbContext> options) : base(options)
     {
-        public ProdutoDbContext(DbContextOptions<ProdutoDbContext> options) : base(options)
-        {
-            
-        }
-        public DbSet<Categoria> Categorias {get; set;}
-        public DbSet<Produto> Produtos {get; set;}
+        
     }
+    public DbSet<Categoria> Categorias {get; set;}
+    public DbSet<Produto> Produtos {get; set;}
+
+    //Fluent API
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+
+        base.OnModelCreating(mb);
+        
+        //Categoria
+        mb.Entity<Categoria>().HasKey(cp => cp.CategoriaId);
+
+        mb.Entity<Categoria>().
+            Property(cp => cp.Nome).
+                HasMaxLength(100).
+                    IsRequired();
+        //Produto
+        mb.Entity<Produto>().HasKey(cp => cp.ProdutoId);
+
+        mb.Entity<Produto>().
+            Property(cp => cp.Nome).
+                HasMaxLength(100).
+                    IsRequired();
+        mb.Entity<Produto>().
+            Property(cp => cp.Descricao).
+                HasMaxLength(255).
+                    IsRequired();
+        mb.Entity<Produto>().
+            Property(cp => cp.ImagemURL).
+                HasMaxLength(255).
+                    IsRequired();
+        mb.Entity<Produto>().
+            Property(cp => cp.Preco).
+                HasPrecision(12,2).
+                    IsRequired();
+        //Relacionamento
+        mb.Entity<Categoria>()
+            .HasMany(x => x.Produtos)
+            .WithOne(x => x.Categoria)
+            .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        
+        // Seeding
+
+        mb.Entity<Categoria>().HasData(
+            new Categoria
+            {
+                CategoriaId = 1,
+                Nome = "Eletrônicos",
+            },
+            new Categoria
+            {
+                CategoriaId = 2,
+                Nome = "Acessórios",
+            }
+        );
+    } 
 }
--- urbanshop.ProductApi/Models/Produto.cs
+++ urbanshop.ProductApi/Models/Produto.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 namespace urbanshop.ProductApi.Models;
 public class Produto
 {
-    public int Id { get; set; }
+    public int ProdutoId { get; set; }
     public string Nome { get; set; }
     public decimal Preco { get; set; }
     public string? Descricao { get; set; }
     public long  Estoque { get; set; }
-    public string? ImageURL{ get; set; }
+    public string? ImagemURL{ get; set; }
     public Categoria? Categoria { get; set; }
     public int CategoriaId { get; set; }
 }
